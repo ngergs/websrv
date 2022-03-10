957241ee67a0e9bb1bd91d4e0925f6498432a5d9
ngergs <niklas@ngergs.de>
2022-03-10 22:36:35 +0100

    feat: add a simple conditional healthcheck handler

--- server/healthCheck.go
+++ server/healthCheck.go
@@ -8,3 +8,13 @@ func HealthCheckHandler() http.Handler {
 		w.WriteHeader(http.StatusOK)
 	})
 }
+
+// HealthChecConditionalkHandler is a conditional healthcheck handler that returns HTTP 200 when the condition argument function returns true and HTTP 503 if not.
+func HealthCheckConditionalHandler(condition func() bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		if condition() {
+			w.WriteHeader(http.StatusOK)
+		}
+		w.WriteHeader(http.StatusServiceUnavailable)
+	})
+}
